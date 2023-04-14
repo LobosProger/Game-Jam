9821f87fcee3a7f8029702b872cd1066218102af
LobosProger <knyazev.yaroslav.2013@gmail.com>
2023-04-14 09:46:40 +0600

    upd

--- Assets/Scripts/RootCreator/PathEditor.cs
+++ Assets/Scripts/RootCreator/PathEditor.cs
@@ -3,40 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(PathCreator))]
-public class PathEditor : Editor
+//[CustomEditor(typeof(PathCreator))]
+public class PathEditor
 {
-    PathCreator creator;
-    Path path;
+	/*PathCreator creator;
+	Path path;
 
-    private void OnSceneGUI()
-    {
-        Draw();
-    }
+	private void OnSceneGUI()
+	{
+		 Draw();
+	}
 
-    [System.Obsolete]
-    void Draw()
-    {
-        Handles.color = Color.red;
-        for (int i = 0;i<path.NumPoints;i++)
-        {
-            Vector2 newPos = Handles.FreeMoveHandle(path[i], Quaternion.identity, 0.1f, Vector2.zero, Handles.CylinderHandleCap);
-            if(path[i] != newPos)
-            {
-                Undo.RecordObject(creator, "Move Point");
-                path.MovePoint(i, newPos);
-            }
-        }
-    }
+	[System.Obsolete]
+	void Draw()
+	{
+		 Handles.color = Color.red;
+		 for (int i = 0;i<path.NumPoints;i++)
+		 {
+			  Vector2 newPos = Handles.FreeMoveHandle(path[i], Quaternion.identity, 0.1f, Vector2.zero, Handles.CylinderHandleCap);
+			  if(path[i] != newPos)
+			  {
+					Undo.RecordObject(creator, "Move Point");
+					path.MovePoint(i, newPos);
+			  }
+		 }
+	}
 
-    void OnEnable()
-    {
-        Debug.Log("Path created");
-        creator = (PathCreator)target;
+	void OnEnable()
+	{
+		 Debug.Log("Path created");
+		 creator = (PathCreator)target;
 
-        if(creator.path == null)
-            creator.CreatePath();
+		 if(creator.path == null)
+			  creator.CreatePath();
 
-        path = creator.path;
-    }
+		 path = creator.path;
+	}*/
 }
